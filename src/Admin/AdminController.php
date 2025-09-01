7a62ce8cb3585e1a0a65a3ec0c1f6fb6fd146f4e
Mirco Baalmans <mircobaalmans@live.nl>
2025-09-01 13:01:38 +0200

    Add: Base layout and styling for admin page

--- src/Admin/AdminController.php
+++ src/Admin/AdminController.php
@@ -19,12 +19,49 @@ class AdminController
         return self::$instance;
     }
 
-    public function init()
-    {
-        // Admin initialization logic
-    }
-
     public function renderAdminPage(){
-        return "";
+        $settings = array(0,1,2,3);
+        $pages = array(0,1,2,3);
+        ?>
+            <h2 class="adventure-page-title"><?= __("Customize the adventure", TEXT_DOMAIN);  ?> </h2>
+            <div class="adventure-admin">
+                <div class="adventure-pages">
+                    <h3><?= __('Adventure pages', TEXT_DOMAIN) ?></h3>
+                    <div class="adventure-pages-table">
+                        <?php
+                        foreach($pages as $page){
+                        ?>
+                            <div class="page-table-row">
+                                <div>Title</div>
+                                <div>Description</div>
+                                <div>Loot</div>
+                                <div>Monsters</div>
+                                <div>Connections</div>
+                                <div>
+                                    <a href="#"><span class="material-symbols-outlined">edit</span></a>
+                                    <a href="#"><span class="material-symbols-outlined">delete</span></a>
+                                </div>
+                            </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+                <hr/>
+                <div class="adventure-settings">
+                    <?php
+                    foreach($settings as $setting){
+                    ?>
+                        <div class="adventure-setting">
+                            <h3><?= __('Adventure Settings', TEXT_DOMAIN) ?></h3>
+                            <input type="text" placeholder="setting">
+                            <textarea></textarea>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        <?php
     }
 }
